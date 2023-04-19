5ac144b52499390758eced586fd017174738549f
endyd1567 <endyd1567@gmail.com>
2023-04-19 16:46:00 +0900

    8. update,delete 기능 추가

--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -35,12 +35,7 @@ class UserDaoTest {
 
     @Test
     public void insert() throws SQLException {
-        User user = new User();
-        String name = "엄두용";
-        String password = "1234";
-        user.setName(name);
-        user.setPassword(password);
-        userDao.insert(user);
+        User user = insertedUser();
 
         User insertedUser = userDao.findById(user.getId());
         assertThat(insertedUser.getId()).isGreaterThan(1l);
@@ -48,6 +43,42 @@ class UserDaoTest {
         assertThat(insertedUser.getId()).isEqualTo(user.getId());
         assertThat(insertedUser.getName()).isEqualTo(user.getName());
         assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    private User insertedUser() throws SQLException {
+        User user = new User();
+        String name = "엄두용";
+        String password = "1234";
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+        return user;
+    }
+
+    @Test
+    public void update() throws SQLException {
+        User user = insertedUser();
+        String updatedName = "updated_엄두용";
+        String updatedPassword = "updated_1234";
+        user.setName(updatedName);
+        user.setPassword(updatedPassword);
+
+        userDao.update(user);
+        User updatedUser = userDao.findById(user.getId());
+
+        assertThat(updatedUser.getId()).isEqualTo(user.getId());
+        assertThat(updatedUser.getName()).isEqualTo(updatedName);
+        assertThat(updatedUser.getPassword()).isEqualTo(updatedPassword);
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        User user = insertedUser();
+
+        userDao.delete(user.getId());
+        User deletedUser = userDao.findById(user.getId());
+
+        assertThat(deletedUser).isNull();
     }
 
 }
