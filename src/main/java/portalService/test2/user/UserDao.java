fd280a10fe330d286e31d885c9f00694ce12849d
endyd1567 <endyd1567@gmail.com>
2023-04-19 16:26:10 +0900

    6. connectionMaker --> DataSource 변경

--- src/main/java/portalService/test2/user/UserDao.java
+++ src/main/java/portalService/test2/user/UserDao.java
@@ -3,20 +3,22 @@ package portalService.test2.user;
 import portalService.test2.connection.ConnectionMaker;
 import portalService.test2.connection.JejuConnectionMaker;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 import static portalService.test2.connection.ConnectionConst.*;
 
 public class UserDao {
 
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User findById(Long id) throws SQLException {
-        Connection con = connectionMaker.getConnection();
+        Connection con = dataSource.getConnection();
         String sql = "select id,name,password from userinfo where id=?";
         PreparedStatement psmt = con.prepareStatement(sql);
         psmt.setLong(1, id);
@@ -36,7 +38,7 @@ public class UserDao {
     }
 
     public void insert(User user) throws SQLException {
-        Connection con = connectionMaker.getConnection();
+        Connection con = dataSource.getConnection();
         String sql = "insert into userinfo(name,password) values(?,?) ";
         PreparedStatement psmt = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
         psmt.setString(1, user.getName());
