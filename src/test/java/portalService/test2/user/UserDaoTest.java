5afca940f9f4a631d903acb73ce82d6db58bb9c6
endyd1567 <endyd1567@gmail.com>
2023-04-19 16:14:24 +0900

    4. DI 적용 : DaoFactory 생성

--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -17,8 +17,8 @@ class UserDaoTest {
         Long id = 1l;
         String name = "umdu";
         String password = "1234";
-        JejuConnectionMaker jejuConnectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(jejuConnectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.userDao();
         User user = userDao.findById(id);
 
         assertThat(user.getId()).isEqualTo(id);
@@ -33,8 +33,8 @@ class UserDaoTest {
         String password = "1234";
         user.setName(name);
         user.setPassword(password);
-        JejuConnectionMaker jejuConnectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(jejuConnectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.userDao();
         userDao.insert(user);
 
         User insertedUser = userDao.findById(user.getId());
@@ -45,36 +45,4 @@ class UserDaoTest {
         assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
     }
 
-    @Test
-    public void getForHalla() throws SQLException {
-        Long id = 1l;
-        String name = "umdu";
-        String password = "1234";
-        HallaConnectionMaker hallaConnectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(hallaConnectionMaker);
-        User user = userDao.findById(id);
-
-        assertThat(user.getId()).isEqualTo(id);
-        assertThat(user.getName()).isEqualTo(name);
-        assertThat(user.getPassword()).isEqualTo(password);
-    }
-
-    @Test
-    public void insertForHalla() throws SQLException {
-        User user = new User();
-        String name = "엄두용한라";
-        String password = "1234";
-        user.setName(name);
-        user.setPassword(password);
-        HallaConnectionMaker hallaConnectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(hallaConnectionMaker);
-        userDao.insert(user);
-
-        User insertedUser = userDao.findById(user.getId());
-        assertThat(insertedUser.getId()).isGreaterThan(1l);
-
-        assertThat(insertedUser.getId()).isEqualTo(user.getId());
-        assertThat(insertedUser.getName()).isEqualTo(user.getName());
-        assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
-    }
 }
